272df64bf757c993332a222442173de598a46c9d
Chandu <chandu@gmail.com>
2023-01-24 11:16:24 +0000

    finished Day04 task

---
+++ 2023/day04/script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo "The input variables are $1 and $2"
+
---
+++ 2023/day04/script3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $1 -gt $2 ]
+then
+	echo "$1 is the bigger number"
+else
+	echo "$2 is the bigger number"
+fi
+
